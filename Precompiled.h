adb44acbacea7835f93074684d4cad752e3f8f8f
mimos1010 <zwitztheone@gmail.com>
2016-05-24 19:57:57 -0400

    Create Precompiled.h

---
+++ Precompiled.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Main.hpp>
+#include <SFML/Config.hpp>
+
+#include <cstdlib>
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+#include <random>
+#include <string>
+
+
+//Main function declarations
+void instructionText();
+void winClose(sf::RenderWindow &window, sf::Event eventx);
+void handleVars(sf::RenderWindow &window, sf::Event eventx, int &var1, int &var2);
+
+
+
+
+
